0495d0dfb32b41566b629e1ba0eec44bf8d3e57e
zires <zshuaibin@gmail.com>
2011-04-27 00:54:17 +0800

    complete spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,16 @@
-$:.unshift File.expand_path(File.dirname(__FILE__) + "/../lib")
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.dirname(__FILE__)
+
 require 'rspec'
 require 'spongecms'
 require 'rack/test'
