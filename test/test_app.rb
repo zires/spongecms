cc1b5802d419c2acfc704b50a092c1757c94e4a4
zires <zshuaibin@gmail.com>
2011-05-14 15:11:42 +0800

    add test_app and finish layout css

---
+++ test/test_app.rb
@@ -0,0 +1,14 @@
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'spongecms'
+
+class TestApp < Sinatra::Base
+  use Spongecms::Core
+
+  get '/' do
+    'test app'
+  end
+
+  run!
+end
+
+
